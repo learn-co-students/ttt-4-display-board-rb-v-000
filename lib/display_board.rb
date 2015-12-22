1b0f796ffaa4a117611b2ffdcbe1fc8e972d6dcc
Chris Aporta <christopher.aporta@gmail.com>
2015-12-21 22:40:42 -0500

    complete lab

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  top_row = " #{board[0]} | #{board[1]} | #{board[2]} "
+  rule    = '-----------'
+  mid_row = " #{board[3]} | #{board[4]} | #{board[5]} "
+  bot_row = " #{board[6]} | #{board[7]} | #{board[8]} "
+
+  puts top_row
+  puts rule
+  puts mid_row
+  puts rule
+  puts bot_row
+end
