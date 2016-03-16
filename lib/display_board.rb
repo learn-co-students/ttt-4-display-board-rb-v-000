ee32d8c1d6c5b95bb2956b61f1dab688a375f49b
github username <mendelblesofsky@gmail.com>
2016-03-16 09:40:57 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+	board.each_slice(3).with_index(1) do |b, i|
+		puts " #{b[0]} | #{b[1]} | #{b[2]} "
+		puts "-----------" if i < 3
+	end
+end
