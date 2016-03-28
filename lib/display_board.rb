a5665101359111526c2c91beb0b2f501d45db89f
miggyteez <mjtavarez@yahoo.com>
2016-03-28 14:46:55 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,70 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
 def display_board(board)
-  puts "   |   |   
------------
-   |   |   
------------
-   |   |   "
-
-   # puts board_init
-
-   puts "   |   |   
------------
-   | #{board[4]} |   
------------
-   |   |   "
-
-   puts " #{board[0]} |   |   
------------
-   |   |   
------------
-   |   |   "
-
-   puts " X | X | X 
------------
-   |   |   
------------
-   |   |   "
-
-   puts "   |   |   
------------
-   |   |   
------------
- O | O | O "
-
- puts " X |   |   
------------
-   | X |   
------------
-   |   | X "
-
-   puts "   |   | O 
------------
-   | O |   
------------
- O |   |   "
-
-  puts "   |   |   
------------
-   |   |   
------------
- X | O | O "
-
-  puts " X | O | X 
------------
-   |   |   
------------
-   |   |   "
-   puts " O | X | X 
------------
-   |   |   
------------
-   |   |   "
-   puts " O | X | O 
------------
-   |   |   
------------
-   |   |   "
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
