e5e1cafff17a6a09ed052353388cf6d000e5e044
Jenni Wise <wiselyjen@gmail.com>
2016-03-30 23:19:05 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# use ' learn --fail-fast ' for testing
+
+#define method
+def display_board(board)
+
+#build board structure of 3 rows, 3 cells per, with lines between. Put array item in each cell
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " ----------- "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " ----------- "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
