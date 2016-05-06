4e3d00f00fb3fad8c779b2ef3b9cbf3cf2839b20
kbasit03 <kbasit03@gmail.com>
2016-05-06 16:55:33 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+#Tic Tac Toe board respresented by an array with 9 string elements 
+# plus two pipe ( | | )symbloes and Rows are separated by two: -----------
+
+board = [" "," "," ","|", " ", " "," "," | ", " ", " "]
+
+def display_board(board)
+  
+  #join method is used to group the empty array and pipe symbole on a single line
+  puts board [0..8].join
+  puts "-----------"
+  puts board [0..8].join
+  puts "-----------"
+  puts board [0..8].join
+end 
+
+
+display_board (board)
