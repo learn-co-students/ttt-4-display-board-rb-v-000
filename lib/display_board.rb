11c67375f0f99f96ff2ccf99758a95be2ceae376
David Lewitan <dovidlwtn@gmail.com>
2016-09-15 22:18:25 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  3.times do |i|
+    3.times do |j|
+        print " #{board[(i+j) + i*2]} #{j < 2 ? '|' : ''}"
+    end
+    if i < 2
+        puts "\n-----------"
+    end
+  end
+end
