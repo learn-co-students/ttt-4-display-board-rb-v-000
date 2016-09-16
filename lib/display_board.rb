f04a789213a483d86486eeb9abd2396fa9d0aafa
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-09-16 12:55:27 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,4 @@
 board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  print "   " "|""   ""|""   "
-  print "\n-----------\n"
-  print "   " "|""   ""|""   "
-  print "\n-----------\n"
-  print "   " "|""   ""|""   \n"
-end
+  puts " #{board[0]} " "|"" #{board[1]} ""|"" #{board[2]} ""\n-----------\n"" #{board[3]} ""|"" #{board[4]} ""|"" #{board[5]} ""\n-----------\n"" #{board[6]} ""|"" #{board[7]} ""|"" #{board[8]} "
+  end
