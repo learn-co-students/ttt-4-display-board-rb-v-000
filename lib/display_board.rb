9f596c691c291e2239eb245cc0f42e84f51438ce
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-01-14 15:57:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,22 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+
+  boardLength = board.length
+  dashRow = "-----------"
+
+  if boardLength == 0
+    puts "   |   |   "
+    puts dashRow
+    puts "   |   |   "
+    puts dashRow
+    puts "   |   |   "
+
+  else 
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts dashRow
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts dashRow
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+end
