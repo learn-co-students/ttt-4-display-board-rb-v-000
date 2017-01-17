a23d58b757b84d34a4a5066d157c8d6914844f65
James Borocz <thesilentc@me.com>
2017-01-17 21:51:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,3 +7,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+## This was easy as part of the progression. The actual hard part was deciding on how literal the spaces inbetween the '|s' were
+### My guess is that we'll learn how to reference those points in the board to make moves
