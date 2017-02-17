1abfca14e3f14fea2b99fea739cd2b2047620d47
Julie Widman <juliewdmn@gmail.com>
2017-02-17 15:21:05 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = Array.new(9, " ")
+
+def display_board(board) # here we need to make our argument name which will be like a placeholder for the board. That's perfect
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
