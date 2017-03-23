78da2b51f01577f2b8baebab9ecfbb85773392b9
Will Sauer <will@heueso.com>
2017-03-23 01:08:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,26 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (current_board)
+width = 3
+total_cells = 9
+
+
+  current_board.each_index { |i|
+    print " #{current_board[i]} "
+
+    if i+1 == total_cells
+    print "\n"
+
+  elsif (i+1) % width == 0
+    print "\n-----------\n"
+
+    elsif i+1 % width != 0 && i+1 != total_cells
+    print "|"
+
+    end
+
+  }
+
+end
+
+#this_board = ["   ","   ","   ","   "," X ","   ","   ","   ","   "]
