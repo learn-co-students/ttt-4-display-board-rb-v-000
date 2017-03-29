f272062f25e780ec9b28e9e017824fbb8ba07dd2
Keith  Polischuk <kpolischuk@gmail.com>
2017-03-29 21:22:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(cell)
+  display = " #{cell[0]} | #{cell[1]} | #{cell[2]} \n-----------\n #{cell[3]} | #{cell[4]} | #{cell[5]} \n-----------\n #{cell[6]} | #{cell[7]} | #{cell[8]} \n"
+puts display
+end
+
+blank = [" "," "," "," "," "," "," "," "," "]
+toplefto = ["O"," "," "," "," "," "," "," "," "]
+xwins = blank = ["X","X","X"," "," "," "," "," "," "]
+
+display_board(blank)
+
+display_board(toplefto)
+
+display_board(xwins)
