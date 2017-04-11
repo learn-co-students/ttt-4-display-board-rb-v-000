4ecf457ac35ea45c2e37c243ed17bd9591d31c7f
britt <britt.cumens@gmail.com>
2017-04-11 09:00:44 -0400

    started lab

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |    "]
+
+# Interpolate board into rows ?
+
+puts rows
+
+def display_board(board)
+
+
+end
