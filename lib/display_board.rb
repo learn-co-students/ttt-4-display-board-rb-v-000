c741664b309bf103a614b3987c8c8e4dd7bd29f9
Annalisa Graziano <arosegraz@gmail.com>
2017-07-04 17:55:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+    cell = "   "
+    pipe = "|"
+    separating = "-----------"
+    row = cell + pipe + cell + pipe + cell
+    puts row
+    puts separating
+    puts row
+    puts separating
+    puts row
+  end
