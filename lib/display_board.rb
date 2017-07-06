8d45ec211b632c2218d3921e8600b755676fccad
Camille Neuner <neuner.camille@gmail.com>
2017-07-06 06:27:46 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board_empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board_all_x = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+board_all_o = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+def display_board(board)
+  row_dashes = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "#{row_dashes}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "#{row_dashes}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+display_board(board_empty)
