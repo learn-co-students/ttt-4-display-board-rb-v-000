47a8cce88d4f65695fcc2cfe6b7b659c5e5dd65a
Veronica Bustabad <vbustabad@gmail.com>
2017-08-17 02:49:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,43 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+end
+
+def display_board(board)
+  puts "   |   |   \n-----------\n   | #{board[4]} |   \n-----------\n   |   |   \n"
+end
+
+def display_board(board)
+  puts " #{board[0]} |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+end
+
+def display_board(board)
+  puts " #{board[0]} |   |   \n-----------\n   | #{board[4]} |   \n-----------\n   |   |   \n"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n   |   |   \n-----------\n   |   |   \n"
+end
+
+def display_board(board)
+  puts "   |   |   \n-----------\n   |   |   \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
+
+def display_board(board)
+  puts " #{board[0]} |   |   \n-----------\n   | #{board[4]} |   \n-----------\n   |   | #{board[8]} \n"
+end
+
+def display_board(board)
+  puts "   |   | #{board[2]} \n-----------\n   | #{board[4]} |   \n-----------\n #{board[6]} |   |   \n"
+end
+
+def display_board(board)
+  puts "   | #{board[1]} |   \n-----------\n #{board[3]} |   | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
