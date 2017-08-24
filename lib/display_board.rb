727cfba1bb30b911b9bd6de3ee80ba5096e46654
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-08-24 17:03:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(location_of_move = ["   ","   ","   ","   ","   ","   ","   ","   ","   "])
+array_of_location = Array.new(location_of_move)
+row1_square1 = array_of_location[0]
+row1_square2 = array_of_location[1]
+row1_square3 = array_of_location[2]
+row2_square1 = array_of_location[3]
+row2_square2 = array_of_location[4]
+row2_square3 = array_of_location[5]
+row3_square1 = array_of_location[6]
+row3_square2 = array_of_location[7]
+row3_square3 = array_of_location[8]
+puts " #{row1_square1} | #{row1_square2} | #{row1_square3} "
+puts "-----------"
+puts " #{row2_square1} | #{row2_square2} | #{row2_square3} "
+puts "-----------"
+puts " #{row3_square1} | #{row3_square2} | #{row3_square3} "
+end
