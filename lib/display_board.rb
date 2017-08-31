15dbc9a8095d051414fed17f9ad4666a4204dc12
codeofarmour <codeofarmour@outlook.com>
2017-08-31 02:49:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,63 +1,11 @@
-<<<<<<< HEAD
 
 def display_board(board)
-=======
-def display_board(board)
   #puts board
->>>>>>> 754ea5237caa230ff726f78057ff969c2278e7e0
 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-<<<<<<< HEAD
 
 end
-
-#main program -- main universe
-#puts "Welcome to Tic Tac Toe!"
-=======
-end
-
-#puts "Welcome to Tic Tac Toe!"
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
-
-
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-display_board(board)
-
-
-board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
-display_board(board)
-
-
-board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-display_board(board)
-
-
-board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
-display_board(board)
-
-
-board = [" ", " ", "X", " ", "O", " ", " ", "O", "X"]
-display_board(board)
-
-
-board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-display_board(board)
-
-
-board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-display_board(board)
->>>>>>> 754ea5237caa230ff726f78057ff969c2278e7e0
