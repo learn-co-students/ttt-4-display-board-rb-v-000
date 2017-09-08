11329473b1f3b508fcf0aafb8158ad9cc0cf0202
Jonathan Dust <d24dust@hotmail.com>
2017-09-08 20:57:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(array)
+  blank_board = " #{array[0]} | #{array[1]} | #{array[2]} \n-----------\n #{array[3]} | #{array[4]} | #{array[5]} \n-----------\n #{array[6]} | #{array[7]} | #{array[8]} \n"
+
+  print blank_board
+
+end
