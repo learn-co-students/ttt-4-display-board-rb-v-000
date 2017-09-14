827c0e4026f875f3d5ec1e2a9afd4e6615b35e76
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-09-14 14:09:36 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+square1 = " "
+square2 = " "
+square3 = " "
+square4 = " "
+square5 = " "
+square6 = " "
+square7 = " "
+square8 = " "
+square9 = " "
+
+board = [square1, square2, square3, square4, square5, square6, square7, square8, square9]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
