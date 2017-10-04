1f86ba71ccebbd4c76d4be3d5e32ebeb0d66e3a5
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-04 20:57:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  $board = [" "," ","X"," "," "," "," "," "," "]
+  $cell =  "   "
+  $vertical_lines = "|"
+  $horizontal_lines = "-----------"
+      2.times do
+      puts $cell + $vertical_lines + $cell + $vertical_lines + $cell
+      puts $horizontal_lines
+    end
+    puts $cell + $vertical_lines + $cell + $vertical_lines + $cell
+  end
+  # if board = 9 empty strings, print board
+  # else if board has one X, print board with one X 
+
+end
+  display_board($board)
