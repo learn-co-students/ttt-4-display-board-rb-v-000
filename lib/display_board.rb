cb6d17418b0a8f33be70d74f71695c38fa9a26ac
Halie Kastl <haliekastl@gmail.com>
2017-10-16 21:07:24 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,27 @@
 # Define display_board that accepts a board and prints
-# out the current state.
+# out the current state
+def display_board(board)
+  board1 = ["   |   |   ","-----------","   |   |   ","-----------","   |   |   "]
+  vert = "|"
+  line = "-----------"
+  final = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+
+  if board.length == 0
+    puts "#{board1[0]}
+          #{board1[1]}
+          #{board1[2]}
+          #{board1[3]}
+          #{board1[4]}"
+  else
+    i = -1
+    while i <= board.length
+      i+= 1
+      final[i] = " #{board[i]} "
+    end
+    puts "#{final[0]}#{vert}#{final[1]}#{vert}#{final[2]}"
+    puts "#{line}"
+    puts "#{final[3]}#{vert}#{final[4]}#{vert}#{final[5]}"
+    puts "#{line}"
+    puts "#{final[6]}#{vert}#{final[7]}#{vert}#{final[8]}"
+  end
+end
