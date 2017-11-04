c239653ba2e8af7dd541718747fe1920c365cb45
Hanna Vanderloop <hvanderloop@gmail.com>
2017-11-04 20:31:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,4 @@
-board = ["O", "O", "O", "O", "O", "O", "O", "O", "O",]
-
+board = ["", "", "", "", "", "", "", "", ""]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
