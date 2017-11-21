1fb90cbdc2c27b0051a15e9ed0fd9e108d68f3fb
James Ferguson <ncjamesferguson@gmail.com>
2017-11-21 19:15:43 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,28 +1,27 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def square_line_top(board)
   for i in 0..1
-    print board[i]
+    print " #{board[i]} "
     print "|"
   end
-  puts board [2]
+  puts " #{board[2]} "
 end
 
 def square_line_middle(board)
   for i in 3..4
-    print board[i]
+    print " #{board[i]} "
     print "|"
   end
-  puts board [5]
+  puts " #{board[5]} "
 end
 
 def square_line_bottom(board)
   for i in 6..7
-    print board[i]
+    print " #{board[i]} "
     print "|"
   end
-  puts board [8]
+  puts " #{board[8]} "
 end
 
 def line_line
@@ -36,5 +35,3 @@ def display_board(board)
   line_line
   square_line_bottom(board)
 end
-
-display_board(board)
