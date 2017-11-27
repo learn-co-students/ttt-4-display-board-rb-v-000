bc1bf866f0c60b22663d3677c1a28b7dcc797cc7
Aubrey Neimeier <neimeier.1@osu.edu>
2017-11-27 21:31:27 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+#the above board definition is not required
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
