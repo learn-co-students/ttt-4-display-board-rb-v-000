409de43944becd4a7a36002236f7bee493bfdc4d
Robert Watson <spartan83@gmail.com>
2017-12-02 05:04:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (toe=Array.new)
+board =" #{toe[0]} | #{toe[1]} | #{toe[2]} \n-----------\n #{toe[3]} | #{toe[4]} | #{toe[5]} \n-----------\n #{toe[6]} | #{toe[7]} | #{toe[8]} "
+puts board
+end
