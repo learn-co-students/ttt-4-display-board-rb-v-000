311dbce96293d18d31ea5c03ceed3acb952b15b9
Jeamay Palo <jeamay.palo@gmail.com>
2018-01-10 03:31:25 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,54 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+puts "Welcome to Tic-Tac-Toe!"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board(board)
+
+puts "Board 1"
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+puts "Board 2"
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "Board 3"
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+puts "Board 4"
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+puts "Board 5"
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board)
+
+puts "Board 6"
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board)
+
+puts "Board 7"
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board)
+
+puts "Board 8"
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+display_board(board)
+
+puts "Board 9"
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+display_board(board)
+
+puts "Board 10"
+board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+display_board(board)
+
+puts "Board 11"
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board)
