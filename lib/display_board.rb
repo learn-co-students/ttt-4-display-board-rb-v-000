4542d10a609b7e1246fededa2fcb124d5d7528d4
Virginia Herron <inadream@gmail.com>
2018-01-19 06:51:36 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,29 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["","","","","","","","",""]
+
+def display_board(board)
+  cell = "   "
+  pipe = "|"
+  seperator = "-----------"
+  print " #{board[0]} "
+  print pipe
+  print " #{board[1]} "
+  print pipe
+  print " #{board[2]} "
+  puts
+  puts seperator
+  print " #{board[3]} "
+  print pipe
+  print " #{board[4]} "
+  print pipe
+  print " #{board[5]} "
+  puts
+  puts seperator
+  print " #{board[6]} "
+  print pipe
+  print " #{board[7]} "
+  print pipe
+  print " #{board[8]} "
+  puts
+end
