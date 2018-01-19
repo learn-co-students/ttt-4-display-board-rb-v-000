f6c497a2466cb67719b3ef204c5b559756cfcc42
Rima Jean <rima.jean@gmail.com>
2018-01-19 16:43:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,82 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board(board)
-  board = [" "," "," "," ","X"," "," "," "," "]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board(board)
-  board = ["O"," "," "," "," "," "," "," "," "]
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board(board)
-  board = ["O"," "," "," ","X"," "," "," "," "]
-  puts " O |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board(board)
-  board = ["X","X","X"," "," "," "," "," "," "]
-  puts " X | X | X "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board(board)
-  board = [" "," "," "," "," "," ","O","O","O"]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts " O | O | O "
-end
-def display_board(board)
-  board = ["X"," "," "," ","X"," "," "," ","X"]
-  puts " X |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   | X "
-end
-def display_board(board)
-  board = [" "," ","O"," ","O"," ","O"," "," "]
-  puts "   |   | O "
-  puts "-----------"
-  puts "   | O |   "
-  puts "-----------"
-  puts " O |   |   "
-end
-def display_board(board)
-  board = ["X","X","X","X","X","X","X","X","X"]
-  puts " X | X | X "
-  puts "-----------"
-  puts " X | X | X "
-  puts "-----------"
-  puts " X | X | X "
-end
-def display_board(board)
-  board = ["O","O","O","O","O","O","O","O","O"]
-  puts " O | O | O "
-  puts "-----------"
-  puts " O | O | O "
-  puts "-----------"
-  puts " O | O | O "
+ puts  " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts  "-----------"
+ puts  " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts  "-----------"
+ puts  " #{board[6]} | #{board[7]} | #{board[8]} "
 end
