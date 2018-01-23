af5003c8f4686d647d0bf20abd08b126783e9907
Igli Cekani <htnacekan@gmail.com>
2018-01-23 05:11:55 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board=[" O ", " X ","   ","   "," X ","   "," X "," O ","   "]
+
 def display_board (board)
 
   rowseperate="-----------"
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts " #{board[0]} "+"|"+" #{board[1]} "+"|"+" #{board[2]} "
   puts "#{rowseperate}"
-  puts "#{board[3]}|#{board[4]}|#{board[5]}"
+  puts " #{board[3]} "+"|"+" #{board[4]} "+"|"+" #{board[5]} "
   puts "#{rowseperate}"
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+  puts " #{board[6]} "+"|"+" #{board[7]} "+"|"+" #{board[8]} "
 end
-display_board (board)
