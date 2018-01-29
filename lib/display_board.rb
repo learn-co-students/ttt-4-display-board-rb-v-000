2459a0e1087594052b5b13e07895181cb8a766b5
Zac Willmington <zac.willmington@gmail.com>
2018-01-29 22:53:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+
+br = [" "," "," "," "," "," "," "," "," ",]
+
+ def display_board(br)
+  a = br[0]
+  b = br[1]
+  c = br[2]
+  d = br[3]
+  e = br[4]
+  f = br[5]
+  g = br[6]
+  h = br[7]
+  i = br[8]
+  puts " #{a} | #{b} | #{c} "
+  puts     "-----------"
+  puts " #{d} | #{e} | #{f} "
+  puts     "-----------"
+  puts " #{g} | #{h} | #{i} "
+ end
