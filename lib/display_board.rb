5b8b147b4a02c375d87bac75fbebae39c5ac840d
Cedric Niguidula <cedcn-9mail@yahoo.com>
2018-02-02 22:57:59 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(entry)
+  puts " #{entry[0]} | #{entry[1]} | #{entry[2]} "
+  puts "-----------"
+  puts " #{entry[3]} | #{entry[4]} | #{entry[5]} "
+  puts "-----------"
+  puts " #{entry[6]} | #{entry[7]} | #{entry[8]} "
+end
