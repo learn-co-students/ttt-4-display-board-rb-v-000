2d45eb98763454b9ca20a4c20284cbf32bef7ca9
Ethel C <cofresi.ethel@gmail.com>
2018-02-25 21:31:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+# ecofresi
+# Display board based on current state. 
+#
+def display_board(board)
+  
+  spaces = "   "
+  pipe = "|"
+  line = "-----------"
+  
+  row1 = " #{board[0]} " + pipe + " #{board[1]} " + pipe + " #{board[2]} "
+  row2 = " #{board[3]} " + pipe + " #{board[4]} " + pipe + " #{board[5]} " 
+  row3 = " #{board[6]} " + pipe + " #{board[7]} " + pipe + " #{board[8]} " 
+  
+  puts row1
+  puts line
+  puts row2
+  puts line
+  puts row3
+  
+end
