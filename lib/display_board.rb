06cd18659afdad75a99f2978a2d4212e5c7643c1
geoffrey woodcock <theatomproject@gmail.com>
2018-03-28 01:38:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,31 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board =[" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  cell = " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
+  cell_2 = " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+  cell_3 = " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
+  line = "-----------"
+puts cell
+puts line
+puts cell_2
+puts line
+puts cell_3
+end
+
+board =[ " ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(board)
+  cell = " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
+  cell_2 = " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+  cell_3 = " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
+  line = "-----------"
+puts cell
+puts line
+puts cell_2
+puts line
+puts cell_3
+end
+
+
