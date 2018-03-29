bcb91d43faf826241e56266c7f40d94f64d7bb6e
Arnold Su <arnoldtingsu@gmail.com>
2018-03-29 04:10:19 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,13 +4,13 @@
 
 def display_board(board)
 
-
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 puts "Welcome Player 1. Make your move!"
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
