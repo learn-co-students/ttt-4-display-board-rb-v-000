e0785a63ab0fe46e3906dde209dbb0057c2902f9
Dale Terrell <tragicid@gmail.com>
2018-04-06 21:51:59 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,8 @@
-def display_board(row_1) = [" ", " ", " "]
-def display_board(row_2) = [---|---|---]
-def display_board(row_3) = [" ", " ", " "]
-def display_board(row_4) = [---|---|---]
-def display_board(row_5) = [" ", " ", " "]
-  
-puts row_1
-puts row_2
-puts row_3
-puts row_4
-puts row_5
+
+def display_board(board)
+  puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
 end
