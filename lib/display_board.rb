db1a39c1b101714c517967d2456fae8e20a2185a
Angela Campion <campion.angela@gmail.com>
2018-04-07 01:39:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = ["X", "O", "O", " ", " ", "X", "O", "X", " "]
+def display_board(board)
+  row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row_two = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row_three = " #{board[6]} | #{board[7]} | #{board[8]} "
+  separating_line = "-----------"
+  puts row_one
+  puts separating_line
+  puts row_two
+  puts separating_line
+  puts row_three
+  end
