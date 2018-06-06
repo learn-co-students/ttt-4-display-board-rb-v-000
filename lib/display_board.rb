ed9b4dc9aaebddbb5649ce4f40b9ef67216dc8f7
Justine Bricknell <justinegayton@gmail.com>
2018-06-06 18:52:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["0", "X", " ", "X", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
