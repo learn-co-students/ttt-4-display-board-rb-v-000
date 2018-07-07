175d2cc8c94f02bb29cc8d4e62477275953d2af8
Caroline Berg <carolineeberg@gmail.com>
2018-07-07 23:16:35 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board_two = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board_three = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+board_four = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board_five = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board_six = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board_seven = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+board_eight = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+board_nine = [" ", "X", "O", "O", "X", " ", " ", "O", "X"]
+board_ten = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+board_eleven = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+def display_board(board)
+  row_one = [" #{board[0]} | #{board[1]} | #{board[2]} "]
+  row_two = [" #{board[3]} | #{board[4]} | #{board[5]} "]
+  row_three = [" #{board[6]} | #{board[7]} | #{board[8]} "]
+  barrier = ["-----------"]
+  puts row_one
+  puts barrier
+  puts row_two
+  puts barrier
+  puts row_three
+end
