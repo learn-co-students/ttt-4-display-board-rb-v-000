f1007fdc24de148f6738773a4b688939b8471d46
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-18 19:56:44 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,42 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+
+	def display_board(board)
+	    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+	    puts "-----------"
+	    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+	    puts "-----------"
+	    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+	  end
+
+
+
+
+#Interesting code, but not good for whatever it is I need right now:
+
+# Make a board
+#row_1 = Array.new
+#row_2 = Array.new
+#row_3 = Array.new
+#row_4 = Array.new
+#row_5 = Array.new
+#board = Array.new
+
+#row_1 = ["   ","|", "   ", "|", "   "] 
+#row_2 = ["-----------"]
+#row_3 = ["   ","|", "   ", "|", "   "] 
+#row_4 = ["-----------"]
+#row_5 = ["   ","|", "   ", "|", "   "] 
+#board = [row_1, row_2, row_3, row_4, row_5]
+
+# Make board editable
+# will figure this out later
+
+#print row_1
+#print row_2
+#print row_3
+#print row_4
+#print row_5
+# or just print the board, since we made that
