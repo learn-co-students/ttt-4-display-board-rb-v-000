6a34dea517b3c07ac0765a3dc8b0b5750628aa67
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-08-22 01:12:05 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,35 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts "
+     |   |   
+  -----------
+     |   |   
+  -----------
+     |   |   
+     "
+  end
+  
+board = [" "," "," "," "," "," "," "," "," "]
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} 
+-----------
+ #{board[3]} | #{board[4]} | #{board[5]} 
+-----------
+ #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+
+
+
